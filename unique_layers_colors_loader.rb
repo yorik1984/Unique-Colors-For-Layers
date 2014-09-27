a6baf3c19ced372f0b81c33db429d5049d931591
yorik1984 <yorik1984@gmail.com>
2014-09-27 16:31:38 +0300

    test

--- unique_layers_colors_loader.rb
+++ unique_layers_colors_loader.rb
@@ -7,5 +7,5 @@ unique_layers_colors = SketchupExtension.new "Unique Layers Colors", "unique_lay
 unique_layers_colors.copyright= "Copyright 2014 by Yurij Kulchevich"
 unique_layers_colors.creator= "Yurij Kulchevich"
 unique_layers_colors.version = "0.1"
-unique_layers_colors.description = "Plugin make unique all colors of layers in model"
+unique_layers_colors.description = "Plugin makes unique all colors of layers in model"
 Sketchup.register_extension unique_layers_colors, true
